16401603cf73537436d83fd3a932daf3b0a0f460
Rubens Gouveia <rubensgui.gouveia@gmail.com>
2022-10-25 20:38:44 -0300

    adding port

--- src/server.js
+++ src/server.js
@@ -2,11 +2,14 @@ const express = require('express')
 
 const app = express()
 const routes = require('./routes');
+const port = process.env.PORT || 3000
 
 
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3344)
+app.listen(port,()=>{
+    console.log('servidor está up')
+})
 

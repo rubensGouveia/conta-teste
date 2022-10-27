4b7445a9886c660546a438e32a7ed50d83b31e8d
Rubens Gouveia <rubensgui.gouveia@gmail.com>
2022-10-27 13:56:07 -0300

    adding babel

--- src/controller/QrController.ts
+++ src/controller/QrController.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { Request, Response } from 'express';
 import jsdom from 'jsdom'
-import puppeteer from 'puppeteer'
 const { JSDOM } = jsdom;
 
 const store = async (req: Request, res: Response) => {
@@ -35,20 +34,33 @@ const store = async (req: Request, res: Response) => {
 }
 
 const pup = async (req: Request, res: Response) => {
-  const { params: { url } } = req
-  const browser = await puppeteer.launch({
-    headless: true,
-    executablePath: process.env.CHROMIUM_PATH,
-    args: ['--no-sandbox'],
-  });
+  const { params: { url }, query: { token } } = req
 
-  const page = await browser.newPage();
-  await page.goto('www.fazenda.df.gov.br/nfce/consulta');
-  await page.type('#Chave', 'tesre')
+  return await axios.get(`https://ww1.receita.fazenda.df.gov.br/DecVisualizador/Visualiza/${url}?token=${token}`)
+    .then(
+      response => {
+        const { window } = new JSDOM(`${response.data}`);
+        const nl = window.document.body.querySelectorAll('li')
+        const arr = [] as HTMLLIElement[];
 
-  const teste = await page.$eval('body', el => el.innerHTML)
-
-  res.json(teste)
-
+        for (let i = nl.length; i--; arr.unshift(nl[i]));
+        const arrayStrings = arr.map(a => a.textContent?.split(/[\s]{4}/g).filter(value => value))
+        const splited = arrayStrings.map(a => {
+          if (a && a[8]) {
+            return {
+              codigo: a[1].replace(/[\D]/g, ''),
+              nome: a[0].split('-')[0].trim(),
+              quantidade: Number(a[4]),
+              unidadeMedida: a[6].trim(),
+              valorUnitario: Number(a[8].replace(',', '.'))
+            }
+          }
+        }).filter(v => v)
+        return res.json(splited)
+      }
+    )
+    .catch(
+      error => res.json(error)
+    );
 }
 export default { store, pup };
